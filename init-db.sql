79e726c30f5bc9673b0dad5a24ae0af41cf6b557
Tran Quang Huy <157164487+iuh20092731@users.noreply.github.com>
2024-03-31 09:25:50 +0700

    Update init-db.sql

--- init-db.sql
+++ init-db.sql
@@ -1,12 +1,12 @@
-CREATE TABLE IF NOT EXISTS tblbookIUHX (
+CREATE TABLE IF NOT EXISTS tblbookIUH20092731 (
     id serial PRIMARY KEY,
     title varchar NOT NULL,
     author varchar NOT NULL
 );
 
-INSERT INTO tblbook (title, author) VALUES
-  ('DevOps', 'IUHX'),
-  ('Big Data', 'IUHX'),
-  ('Cloud Deployement', 'IUHX'),
-('Data Analysis', 'IUHX'),
-('Block Chain', 'IUHX');
+INSERT INTO tblbookIUH20092731 (title, author) VALUES
+  ('DevOps', 'IUH20092731'),
+  ('Big Data', 'IUH20092731'),
+  ('Cloud Deployement', 'IUH20092731'),
+('Data Analysis', 'IUH20092731'),
+('Block Chain', 'IUH20092731');
